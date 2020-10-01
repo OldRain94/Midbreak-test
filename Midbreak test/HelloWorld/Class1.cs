3724df4b680165384b5ff23f9722cb5ef99c699f
Kaiqi Zhuang <kzhu0012@student.monash.edu>
2020-10-01 16:19:05 +1000

    Add project files.

---
+++ Midbreak test/HelloWorld/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Midbreak_test.HelloWorld
+{
+    public class Class1
+    {
+        public class Hello
+        {
+            public String GetHello()
+            {
+                return "Hello World!";
+            }
+        }
+    }
+}
